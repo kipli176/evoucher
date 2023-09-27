d3181fbfd6c2abc56b3b285136fd8569aed4fe13
kipli176 <kipli176@yahoo.com>
2023-09-28 03:47:14 +0700

    asdjj

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -14,12 +14,12 @@ class Home extends BaseController
     }
     public function nopol($id)
     {
-        helper(['mongo']);
-        $cookie = file_get_contents(WRITEPATH.'cache/coki.txt');
-        $sesi = file_get_contents(WRITEPATH.'cache/sesi.txt');
-        $post = file_get_contents(WRITEPATH.'cache/post.txt');
+        // helper(['mongo']);
+        // $cookie = file_get_contents(WRITEPATH.'cache/coki.txt');
+        // $sesi = file_get_contents(WRITEPATH.'cache/sesi.txt');
+        // $post = file_get_contents(WRITEPATH.'cache/post.txt');
 
         // echo pertama();
-        echo formCari($sesi, $post, $cookie,$id);
+        // echo formCari($sesi, $post, $cookie,$id);
     }
 }
