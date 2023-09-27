fe9ba056e53a013810bbff51c6826ec1e239e629
kipli176 <kipli176@yahoo.com>
2023-09-27 22:13:12 +0700

    aaa

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,22 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
         return view('index');
+    }
+    public function verif()
+    {
+        return view('verif');
+    }
+    public function nopol($id)
+    {
+        helper(['mongo']);
+        $cookie = file_get_contents(WRITEPATH.'cache/coki.txt');
+        $sesi = file_get_contents(WRITEPATH.'cache/sesi.txt');
+        $post = file_get_contents(WRITEPATH.'cache/post.txt');
+
+        echo pertama();
+        // echo formCari($sesi, $post, $cookie,$id);
     }
 }
--- app/Views/index.php
+++ app/Views/index.php
@@ -1,22 +1,18 @@
 <!doctype html>
-<html lang="en">
+<html lang="id">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="/img/logo.png" type="image/png">
-    <title>Eatsome - Restaurant & Food Delivery Mobile Template</title>
+    <title>eVoucher Jasa Raharja</title>
  
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="/icofont/icofont.min.css">
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css">
-
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-
-    <!-- <link href="https://askbootstrap.com/preview/eatsome/vender/sidebar/demo.css" rel="stylesheet"> --> 
     <link href="/css/demo.css" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
     
@@ -777,17 +773,12 @@
         </ul>
     </nav>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
-        type="text/javascript"></script> 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"
-        type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" type="text/javascript"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" type="text/javascript"></script>
 
-    <script src="/js/hc-offcanvas-nav.js"
-        type="text/javascript"></script>
-
-    <script src="/js/custom.js" type="text/javascript">
-    </script>
+    <script src="/js/hc-offcanvas-nav.js" type="text/javascript"></script>
+    <script src="/js/custom.js" type="text/javascript"></script>
 </body>
 
 </html>
